29d1c1fc5cbce97ca5681c80914679823009a78d
Grant Zargaryan <g.zargaryan@rambler-co.ru>
2023-09-14 00:46:10 +0300

    ios keyboard scroll 3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import { createRoot } from 'react-dom/client';
 import './index.css'
 import App from './components/App';
 
+let initialHeight = window.innerHeight;
+
+window.addEventListener('resize', function() {
+    const currentHeight = window.innerHeight;
+
+    if (currentHeight < initialHeight) {
+        document.body.classList.add("keyboard-opened")
+    } else {
+        document.body.classList.remove("keyboard-opened")
+    }
+
+    initialHeight = currentHeight;
+});
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
